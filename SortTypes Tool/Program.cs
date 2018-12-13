a5ca52ccfffff35406686a54635303d56d058962
Kamel El Yousefy <elyousefy.hamza@gmx.com>
2018-12-13 17:30:47 +0100

    V0.1

--- SortTypes Tool/Program.cs
+++ SortTypes Tool/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,29 @@ namespace SortTypes_Tool
     {
         static void Main(string[] args)
         {
+            Console.Title = "SortTypes Tool";
+            Console.WriteLine("Enter the path (ex: C:\\Users\\):");
+
+            string filepath = Console.ReadLine();
+            DirectoryInfo d = new DirectoryInfo(filepath);
+
+
+            foreach (var file in d.GetFiles("*.*"))
+            {
+                string filename = file.Name;
+                string[] filenames = filename.Split('.');
+
+                string extension = filenames[filenames.Length - 1];
+                Directory.CreateDirectory(@"C:\SortTypes\" + extension);
+                Console.WriteLine("Copy in progress....");
+                Console.WriteLine(filename);
+                File.Copy(filepath + @"\" + filename, @"C:\SortTypes\" + extension + @"\" + filename, true);
+                Console.WriteLine("Copy file successfully !");
+                Console.WriteLine("");
+            }
             //code
+            Console.WriteLine("Finish !");
+            Console.ReadKey();
         }
     }
 }
